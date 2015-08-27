87cea0948f2becc7442761d7e0258e4834ed6da7
Mathieu Poussin <mathieu.poussin@netyxia.net>
2015-08-27 22:56:27 +0200

    Package upgrade

--- elpa/evil-easymotion-20150823.2011/evil-easymotion-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-easymotion" "20150823.2011" "A port of vim's easymotion to emacs" '((emacs "24") (avy "20150508.1418")) :url "https://github.com/pythonnut/evil-easymotion.el" :keywords '("convenience" "evil"))
