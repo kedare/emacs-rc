422a16eafde79fa0d1e9fa6dc0775bb65496d25d
Mathieu Poussin <mathieu.poussin@capside.com>
2015-09-18 14:04:46 +0200

    Disable SMEX

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
 (load "~/.emacs.d/my-erc")
 (load "~/.emacs.d/my-evil")
 (load "~/.emacs.d/my-helm")
-(load "~/.emacs.d/my-smex")
+;; (load "~/.emacs.d/my-smex")
 (load "~/.emacs.d/my-perl")
 (load "~/.emacs.d/my-python")
 (load "~/.emacs.d/my-ruby")
