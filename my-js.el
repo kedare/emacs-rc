8a4126a9c5af570611c50ecb37da4c2eb2c25d87
Mathieu Poussin <mathieu.poussin@netyxia.net>
2015-09-24 08:45:45 +0200

    Better JS support ?

--- my-js.el
+++ my-js.el
@@ -8,4 +8,36 @@
 (add-hook 'js2-mode-hook
           (lambda () (flycheck-mode t)))
 
+(add-hook 'js2-mode-hook 'js2-refactor-mode)
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
+(setq-default js2-allow-rhino-new-expr-initializer nil)
+(setq-default js2-auto-indent-p nil)
+(setq-default js2-enter-indents-newline nil)
+(setq-default js2-global-externs '("module" "require" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON"))
+(setq-default js2-idle-timer-delay 0.1)
+(setq-default js2-indent-on-enter-key nil)
+(setq-default js2-mirror-mode nil)
+(setq-default js2-strict-inconsistent-return-warning nil)
+(setq-default js2-auto-indent-p t)
+(setq-default js2-include-rhino-externs nil)
+(setq-default js2-include-gears-externs nil)
+(setq-default js2-concat-multiline-strings 'eol)
+(setq-default js2-rebind-eol-bol-keys nil)
+
+;; Let flycheck handle parse errors
+(setq-default js2-show-parse-errors nil)
+(setq-default js2-strict-missing-semi-warning nil)
+(setq-default js2-strict-trailing-comma-warning t) ;; jshint does not warn about this now for some reason
+
+(add-hook 'js2-mode-hook (lambda () (flycheck-mode 1)))
+
+(require 'js2-refactor)
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
+
+(eval-after-load 'auto-complete
+  '(eval-after-load 'tern
+     '(progn
+        (require 'tern-auto-complete)
+        (tern-ac-setup)))) 
