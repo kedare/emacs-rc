5e77bf03dac000bd0eaf922ce5e8fe2c4541e725
Mathieu Poussin <mathieu.poussin@capside.com>
2015-09-30 09:48:06 +0200

    Package upgrade

--- elpa/avy-20150916.2322/avy-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "avy" "20150916.2322" "set-based completion" '((emacs "24.1") (cl-lib "0.5")) :url "https://github.com/abo-abo/avy" :keywords '("point" "location"))
--- elpa/leuven-theme-20150818.556/leuven-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "leuven-theme" "20150818.556" "Awesome Emacs color theme on white background" 'nil :url "https://github.com/fniessen/emacs-leuven-theme" :keywords '("color" "theme"))
