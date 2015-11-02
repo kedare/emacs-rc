2431f715595ec9cbedb4f27b8b82154fbb3b99c6
Mathieu Poussin <mathieu.poussin@capside.com>
2015-11-02 16:53:59 +0100

    Package upgrade and all elpy

--- my-perl.el
+++ my-perl.el
@@ -2,6 +2,11 @@
 
 (defalias 'perl-mode 'cperl-mode)
 (require 'plsense)
+
+(setq plsense-popup-help-key "C-:")
+(setq plsense-display-help-buffer-key "M-:")
+(setq plsense-jump-to-definition-key "C->")
+
 (plsense-config-default)
 (setq cperl-indent-level 2)
 (custom-set-variables
--- my-python.el
+++ my-python.el
@@ -6,4 +6,6 @@
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
+(elpy-enable)
+
 ;; END OF PYTHON
