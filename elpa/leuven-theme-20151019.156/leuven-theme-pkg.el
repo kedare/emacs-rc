87f204581b244671627ee1d27242b8057e7b75af
Mathieu Poussin <mathieu.poussin@capside.com>
2015-11-12 16:59:10 +0100

    Package upgrade

--- elpa/leuven-theme-20151019.156/leuven-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "leuven-theme" "20151019.156" "Awesome Emacs color theme on white background" 'nil :url "https://github.com/fniessen/emacs-leuven-theme" :keywords '("color" "theme"))
