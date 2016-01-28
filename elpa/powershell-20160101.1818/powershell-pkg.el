dd11ecaa7a3c547e29c76e1676d52ffa60c40110
Mathieu Poussin <mathieu.poussin@capside.com>
2016-01-28 09:22:54 +0100

    Package upgrade

--- elpa/powershell-20160101.1818/powershell-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "powershell" "20160101.1818" "Mode for editing Powershell scripts" '((emacs "24")) :url "http://github.com/jschaf/powershell.el" :keywords '("powershell" "languages"))
