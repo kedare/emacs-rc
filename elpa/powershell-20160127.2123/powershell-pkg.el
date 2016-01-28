b21155fdeb61f0cde4c3956053833c5a87f89e35
Mathieu Poussin <mathieu.poussin@capside.com>
2016-01-28 09:57:42 +0100

    Add missed files

---
+++ elpa/powershell-20160127.2123/powershell-pkg.el
@@ -0,0 +1,1 @@
+(define-package "powershell" "20160127.2123" "Mode for editing Powershell scripts" '((emacs "24")) :url "http://github.com/jschaf/powershell.el" :keywords '("powershell" "languages"))
---
+++ elpa/websocket-20160124.2020/websocket-autoloads.el
@@ -0,0 +1,15 @@
+;;; websocket-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("websocket.el") (22184 45406 669847 233000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; websocket-autoloads.el ends here
