ab75cf8e94d03b3262e600e091eacd22eaf71585
Mathieu Poussin <mathieu.poussin@capside.com>
2016-06-13 10:29:26 +0200

    Package update

---
+++ elpa/dash-functional-20160601.1018/dash-functional-autoloads.el
@@ -0,0 +1,16 @@
+;;; dash-functional-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash-functional.el") (22366 28301 54130
+;;;;;;  633000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-functional-autoloads.el ends here
