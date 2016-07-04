589f7a84c0bceaf362b8f34df01dcf947a95c698
Mathieu Poussin <mathieu.poussin@capside.com>
2016-07-04 10:03:13 +0200

    Disabling not commonly used my- files, updating packages

--- elpa/leuven-theme-20160621.1407/leuven-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "leuven-theme" "20160621.1407" "Awesome Emacs color theme on white background" 'nil :url "https://github.com/fniessen/emacs-leuven-theme" :keywords '("color" "theme"))
